f26badadb85aa62031af64558f043576ae19de5f
Joao Palma <joao.condepalma@gmail.com>
2023-05-06 18:04:14 +0100

    Changed about img size for smaller screens

--- scripts.js
+++ scripts.js
@@ -1,40 +1,23 @@
 var tablinks = document.getElementsByClassName('tab-links');
-        var tabcontents = document.getElementsByClassName('tab-contents');
+var tabcontents = document.getElementsByClassName('tab-contents');
 
-        function opentab(tabname){
-            for(tablink of tablinks){
-                tablink.classList.remove('active-link');
-            }
-            for(tabcontent of tabcontents){
-                tabcontent.classList.remove('active-tab');
-            }
-            event.currentTarget.classList.add('active-link')
-            document.getElementById(tabname).classList.add('active-tab')
-        }
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link')
+    document.getElementById(tabname).classList.add('active-tab')
+}
 
-        var sidemenu = document.getElementById("sidemenu");
+var sidemenu = document.getElementById("sidemenu");
 
-        function openmenu(){
-            sidemenu.style.right = "0";
-        }
+function openmenu(){
+    sidemenu.style.right = "0";
+}
 
-        function closemenu(){
-            sidemenu.style.right = "-200px";
-        }
-
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbx-nbVC6sXidW_1cJv_7yVnCsDT_Y8_c6GykTGb8tBRKzMT9IHJP4_n8BNrf4mUVl3Dew/exec'
-    const form = document.forms['submit-to-google-sheet']
-    const msg = document.getElementById("msg")
-
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function(){
-                msg.innerHTML= ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-    })
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
